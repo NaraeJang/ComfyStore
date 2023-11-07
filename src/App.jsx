b6c40d8dbc612d0792c0851da1556e440203db6a
NaraeJang <j.naaaarae@gmail.com>
2023-11-07 15:09:48 -0500

    SingleProduct.jsx setup 01

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import { ErrorElement } from "./components";
 
 // loaders
 import { loader as landingLoader } from "./pages/Landing";
+import { loader as singleProductLoader } from "./pages/SingleProduct";
 
 // actions
 
@@ -45,6 +46,8 @@ const router = createBrowserRouter([
       {
         path: "products/:id",
         element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader,
       },
       {
         path: "orders",
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,4 +1,20 @@
+import { useLoaderData, Link } from "react-router-dom";
+import { customFetch, formatPrice } from "../utils";
+import { useState } from "react";
+
+export const loader = async ({ params }) => {
+  const response = await customFetch(`/products/${params.id}`);
+
+  return { product: response.data.data };
+};
+
 const SingleProduct = () => {
+  const { product } = useLoaderData();
+  const { title, company, image, price, description, colors } =
+    product.attributes;
+
+  const dollarAmount = formatPrice(price);
+
   return <div>SingleProduct</div>;
 };
 export default SingleProduct;
