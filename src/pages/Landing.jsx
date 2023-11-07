8a0d871c430e4200cce2865cf44984321cdae96d
NaraeJang <j.naaaarae@gmail.com>
2023-11-07 14:06:14 -0500

    ProductGrid.jsx FormatPrice Function

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import { Hero } from "../components";
+import { FeaturedProducts, Hero } from "../components";
 
 import { customFetch } from "../utils";
 
@@ -14,6 +14,7 @@ const Landing = () => {
   return (
     <>
       <Hero />
+      <FeaturedProducts />
     </>
   );
 };
