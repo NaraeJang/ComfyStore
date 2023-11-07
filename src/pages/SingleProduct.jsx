18167f85ecc158568b2768fab21e85b123d4c55f
NaraeJang <j.naaaarae@gmail.com>
2023-11-07 15:52:32 -0500

    SingleProduct.jsx Setup 02

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -12,9 +12,62 @@ const SingleProduct = () => {
   const { product } = useLoaderData();
   const { title, company, image, price, description, colors } =
     product.attributes;
-
   const dollarAmount = formatPrice(price);
+  const [productColor, setProductColor] = useState(colors[0]);
 
-  return <div>SingleProduct</div>;
+  return (
+    <section>
+      <div className="text-md breadcrumbs">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </div>
+      {/* PRODUCT */}
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        {/* IMAGE */}
+        <img
+          src={image}
+          alt={title}
+          className="w-96 h-96 object-cover rounded-lg lg:w-full"
+        />
+
+        {/* PRODUCT INFO */}
+        <div>
+          <h1 className="capitalize text-3xl font-bold">{title}</h1>
+          <h4 className="text-xl text-neutral-content font-bold mt-2">
+            {company}
+          </h4>
+          <p className="mt-3 text-xl">{dollarAmount}</p>
+          <p className="mt-6 leading-8">{description}</p>
+
+          {/* COLORS */}
+          <div className="mt-6">
+            <h4 className="text-md font-medium tracking-wider capitalize">
+              colors
+            </h4>
+            <div className="mt-2">
+              {colors.map((color) => {
+                return (
+                  <button
+                    type="button"
+                    key={color}
+                    className={`badge w-6 h-6 mr-2 ${
+                      color === productColor && "border-2 border-secondary"
+                    }`}
+                    style={{ backgroundColor: color }}
+                    onClick={() => setProductColor(color)}></button>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default SingleProduct;
