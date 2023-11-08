c5e651e4f5d76063dec3305a4d9f9df5fabd7e93
NaraeJang <j.naaaarae@gmail.com>
2023-11-08 18:57:02 -0500

    FormCheckbox.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as FormInput } from "./FormInput";
 export { default as FormSelect } from "./FormSelect";
+export { default as FormCheckbox } from "./FormCheckbox";
 export { default as SubmitBtn } from "./SubmitBtn";
 export { default as Header } from "./Header";
 export { default as Navbar } from "./Navbar";
