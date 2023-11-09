aae42058cb1160896716992c9a8b479b6775b54b
NaraeJang <j.naaaarae@gmail.com>
2023-11-08 20:37:52 -0500

    Make filter functions work together

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Comfy Store is a project that I completed while taking a course of React. In addition, I improved the application with extra codes in order to make it more responsive both mobile and web. Main feature is you can experience the same as when you shop on online shopping mall. you can filter products, save in the cart, see the total price when you check out, register or login as a member of the shopping mall and experience more feature when you login such as checking your order.
 
+!ATTENTION!
+If you use all the filters in Products page, you might find no product exists. It's not an error. It's because the api doesn't contains many items in the list.
+
 ## Libraries & FramEWorks
 
 - Axios
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,7 +5,8 @@ import FormRange from "./FormRange";
 import FormCheckbox from "./FormCheckbox";
 
 const Filters = () => {
-  const { products, meta } = useLoaderData();
+  const { products, meta, params } = useLoaderData();
+  const { search, company, category, order, price, shipping } = params;
 
   return (
     <Form className=" bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
@@ -15,6 +16,7 @@ const Filters = () => {
         label="search product"
         name="search"
         size="input-sm"
+        defaultValue={search}
       />
       {/* CATEGORY */}
       <FormSelect
@@ -22,6 +24,7 @@ const Filters = () => {
         name="category"
         list={meta.categories}
         size="select-sm"
+        defaultValue={category}
       />
       {/* COMPANY */}
       <FormSelect
@@ -29,6 +32,7 @@ const Filters = () => {
         name="company"
         list={meta.companies}
         size="select-sm"
+        defaultValue={company}
       />
       {/* ORDER */}
       <FormSelect
@@ -36,15 +40,21 @@ const Filters = () => {
         name="order"
         list={["a-z", "z-a", "high", "low"]}
         size="select-sm"
+        defaultValue={order}
       />
       {/* PRICE */}
-      <FormRange label="select Price" name="price" size="range-sm" />
+      <FormRange
+        label="select Price"
+        name="price"
+        size="range-sm"
+        price={price}
+      />
       {/* FREE DELIVERY */}
       <FormCheckbox
         label="free shipping"
         name="shipping"
         size="checkbox-sm"
-        // defaultValue="true"
+        defaultValue={shipping}
       />
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm">
