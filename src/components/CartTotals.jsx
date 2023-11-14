15d4984f670652dfe89e77cec051364435fbc6d1
NaraeJang <j.naaaarae@gmail.com>
2023-11-14 14:51:15 -0500

    CartTotals.jsx setup

--- src/components/CartTotals.jsx
+++ src/components/CartTotals.jsx
@@ -1,4 +1,36 @@
+import { useSelector } from "react-redux";
+import { formatPrice } from "../utils";
+
 const CartTotals = () => {
-  return <div>CartTotals</div>;
+  const { cartTotal, shipping, tax, orderTotal } = useSelector((state) => {
+    return state.cartState;
+  });
+
+  return (
+    <div className="card bg-base-200">
+      <div className="card-body">
+        {/* SUBTOTAL */}
+        <p className="cart-totals">
+          <span>Subtotal</span>
+          <span className="font-medium">{formatPrice(cartTotal)}</span>
+        </p>
+        {/* SHIPPING */}
+        <p className="cart-totals">
+          <span>Shipping</span>
+          <span className="font-medium">{formatPrice(shipping)}</span>
+        </p>
+        {/* TAX */}
+        <p className="cart-totals">
+          <span>Tax</span>
+          <span className="font-medium">{formatPrice(tax)}</span>
+        </p>
+        {/* TOTAL */}
+        <p className="cart-totals">
+          <span className="font-bold">Order Total</span>
+          <span className="font-bold">{formatPrice(orderTotal)}</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 export default CartTotals;
