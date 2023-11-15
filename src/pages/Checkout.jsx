6aa9ec7986f68f60f5da8dd0cead8a6c930e77e5
NaraeJang <j.naaaarae@gmail.com>
2023-11-15 16:49:13 -0500

    checkout.jsx setup

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,20 @@
+import { useSelector } from "react-redux";
+import { CartTotals, SectionTitle, CheckoutForm } from "../components";
+
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const cartTotal = useSelector((state) => state.cartState.cartTotal);
+
+  if (cartTotal === 0) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Place your order" />
+      <div className="mt-8 grid gap-8 item-start md:grid-cols-2">
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
 };
 export default Checkout;
